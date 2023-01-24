a96cf6dbfc10b3d61f39c4ce70ba2bff3d034a4e
djbambi <djbambrough@.com>
2023-01-24 16:24:20 +0000

    modified:   src/__pycache__/select_movies.cpython-311.pyc updated 	modified:   src/select_movies.py updated 	modified:   test/__pycache__/test_select_movies.cpython-311-pytest-7.2.1.pyc updated 	modified:   test/test_select_movies.py updated

--- src/select_movies.py
+++ src/select_movies.py
@@ -8,6 +8,23 @@ def selectmovies(username, database):
         conn = pg.Connection(username, database=database)
     except:
         return 'Check username and/or database name.'
-
+    movie_dictionaries = []
+    result = conn.run('SELECT * FROM movies')
     
-
+        
+    
+    titles = [ meta_data['name'] for meta_data in conn.columns]
+    for row in result:
+            movie_dictionaries.append(
+                {
+                titles[0] : row[0],
+                titles[1] : row[1],
+                titles[2] : row[2],
+                titles[3] : row[3],
+                titles[4] : row[4],
+                titles[5] : row[5]
+                }
+                )
+    return movie_dictionaries
+    print(movie_dictionaries[0].keys())
+    
--- test/test_select_movies.py
+++ test/test_select_movies.py
@@ -10,3 +10,10 @@ def test_catches_error_returns_message_if_incorrect_database():
     expected = 'Check username and/or database name.'
     result = selectmovies('northcoders', 'nc_fli')
     assert result == expected
+
+def test_returns_correct_keys_from_movies_table():
+    returned_list = selectmovies('northcoders', 'nc_flix')
+    expected = ['movie_id', 'title', 'release_date', 'rating', 'cost', 'classification']
+    
+    result = list(returned_list[0].keys())
+    assert result == expected
